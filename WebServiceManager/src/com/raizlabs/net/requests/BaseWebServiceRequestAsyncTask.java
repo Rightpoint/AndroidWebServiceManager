1341792ceef5e285f84ad28f3f63b8308ef77e07
Dylan James <Dylan.James@raizlabs.com>
2012-08-21 13:31:33 -0400

    Updated WebServiceRequestAsyncTask to be abstract and allow for non-inline request construction for easier extension, and renamed it to BaseWebServiceRequestAsyncTask. Created another WebServiceRequestAsyncTask which implements the base, and requires a request to be passed to it to keep it easy to build an AsyncTask around a WebServiceRequest.

---
+++ WebServiceManager/src/com/raizlabs/net/requests/BaseWebServiceRequestAsyncTask.java
@@ -0,0 +1,106 @@
+package com.raizlabs.net.requests;
+
+import com.raizlabs.events.ProgressListener;
+import com.raizlabs.net.webservicemanager.RequestMode;
+import com.raizlabs.net.webservicemanager.ResultInfo;
+import com.raizlabs.net.webservicemanager.WebServiceManager;
+import com.raizlabs.net.webservicemanager.WebServiceProgress;
+import com.raizlabs.tasks.RZAsyncTask;
+
+/**
+ * Implementation of an {@link RZAsyncTask} which performs an
+ * {@link WebServiceRequest} using a given {@link WebServiceManager}
+ * and returns the result. This class is abstract, and allows sub
+ * classes to just implement the request creation. To simply use an
+ * exisint request, see {@link WebServiceRequestAsyncTask}.
+ * <br><br> 
+ * Allows for cancellation via {@link #cancelRequest()}, though the
+ * request can always be cancelled via {@link WebServiceRequest#cancel()}.
+ * <br><br>
+ * By default, this will use the default {@link RequestMode} on the
+ * {@link WebServiceManager}, though it can be set manually via
+ * {@link #setRequestMode(RequestMode)}.
+ * 
+ * @author Dylan James
+ *
+ * @param <ResultType> The type of result from the {@link WebServiceRequest}.
+ */
+public abstract class BaseWebServiceRequestAsyncTask<ResultType> extends RZAsyncTask<Void, WebServiceProgress, ResultInfo<ResultType>> {
+	
+	private WebServiceManager webServiceManager;
+	
+	private WebServiceRequest<ResultType> request;
+	/**
+	 * Method called to create the request. This may be called on the UI thread,
+	 * so creation should be quick. Subclasses should call {@link #getRequest()}
+	 * to obtain the request, as it will call this method
+	 * and store the result.
+	 * @return The {@link WebServiceRequest} which should be executed.
+	 */
+	protected abstract WebServiceRequest<ResultType> createRequest();
+	/**
+	 * Gets the {@link WebServiceRequest} to be executed. This calls
+	 * {@link #createRequest()} and stores the result so it only needs
+	 * to be called once.
+	 * @return The {@link WebServiceRequest} to be executed.
+	 */
+	protected WebServiceRequest<ResultType> getRequest() {
+		if (request == null) {
+			request = createRequest();
+		}
+		return request;
+	}
+
+	private RequestMode requestMode;
+	/**
+	 * Sets the {@link RequestMode} to use to make the request via the
+	 * {@link WebServiceManager}.
+	 * @param mode The {@link RequestMode} to use, or null to use the default.
+	 */
+	public void setRequestMode(RequestMode mode) {
+		this.requestMode = mode;
+	}
+	
+	public BaseWebServiceRequestAsyncTask(WebServiceManager manager) {
+		this.webServiceManager = manager;
+	}
+
+	/**
+	 * Cancels this task by cancelling the {@link WebServiceRequest} and then
+	 * cancelling this {@link BaseWebServiceRequestAsyncTask}. 
+	 */
+	public void cancelRequest() {
+		request.cancel();
+		this.cancel(true);
+	}
+	
+	@Override
+	protected boolean doPreExecute() {
+		// This will be instant if the request is null
+		return getRequest() == null;
+	}
+	
+	@Override
+	protected ResultInfo<ResultType> doInBackground(Void... params) {
+		// If we have no request, abort.
+		if (getRequest() == null) {
+			return null;
+		}
+		
+		final BaseWebServiceProgress progress = new BaseWebServiceProgress(-1, -1);
+		request.addProgressListener(new ProgressListener() {
+			@Override
+			public void onProgressUpdate(long currentProgress, long maxProgress) {
+				progress.currentProgress = currentProgress;
+				progress.maxProgress = maxProgress;
+				publishProgress(progress);
+			}
+		});
+		
+		if (isCancelled()) {
+			request.cancel();
+		}
+		
+		return webServiceManager.doRequest(request, requestMode);
+	}
+}
--- WebServiceManager/src/com/raizlabs/net/requests/WebServiceRequestAsyncTask.java
+++ WebServiceManager/src/com/raizlabs/net/requests/WebServiceRequestAsyncTask.java
@@ -1,78 +1,35 @@
 package com.raizlabs.net.requests;
 
-import com.raizlabs.events.ProgressListener;
-import com.raizlabs.net.webservicemanager.RequestMode;
-import com.raizlabs.net.webservicemanager.ResultInfo;
 import com.raizlabs.net.webservicemanager.WebServiceManager;
-import com.raizlabs.net.webservicemanager.WebServiceProgress;
-import com.raizlabs.tasks.RZAsyncTask;
 
 /**
- * Implementation of an {@link RZAsyncTask} which performs an
- * {@link WebServiceRequest} using a given {@link WebServiceManager}
- * and returns the result. Also allows for cancellation via
- * {@link #cancelRequest()}, though the request can always be cancelled via 
- * {@link WebServiceRequest#cancel()}.
- * <br><br>
- * By default, this will use the default {@link RequestMode} on the
- * {@link WebServiceManager}, though it can be set manually via
- * {@link #setRequestMode(RequestMode)}.
+ * Implementation of {@link BaseWebServiceRequestAsyncTask} which allows a
+ * {@link WebServiceRequest} to be provided explicitly. Subclasses will be
+ * required to build the request immediately upon construction. See
+ * {@link BaseWebServiceRequestAsyncTask} for easier extension.
  * 
  * @author Dylan James
  *
- * @param <ResultType> The type of result from the {@link WebServiceRequest}.
+ * @param <ResultType>
  */
-public class WebServiceRequestAsyncTask<ResultType> extends RZAsyncTask<Void, WebServiceProgress, ResultInfo<ResultType>> {
-	
-	private WebServiceRequest<ResultType> request;
-	private WebServiceManager webServiceManager;
+public class WebServiceRequestAsyncTask<ResultType> extends BaseWebServiceRequestAsyncTask<ResultType>{
 
-	private RequestMode requestMode;
-	/**
-	 * Sets the {@link RequestMode} to use to make the request via the
-	 * {@link WebServiceManager}.
-	 * @param mode The {@link RequestMode} to use, or null to use the default.
-	 */
-	public void setRequestMode(RequestMode mode) {
-		this.requestMode = mode;
-	}
+	private WebServiceRequest<ResultType> request;
 	
 	/**
-	 * Creates a {@link WebServiceRequestAsyncTask} which will execute the
+	 * Creates a {@link BaseWebServiceRequestAsyncTask} which will execute the
 	 * given {@link WebServiceRequest} via the given {@link WebServiceManager}.
 	 * @param request The {@link WebServiceRequest} to perform.
 	 * @param manager The {@link WebServiceManager} to use.
 	 */
 	public WebServiceRequestAsyncTask(WebServiceRequest<ResultType> request, WebServiceManager manager) {
+		super(manager);
 		this.request = request;
-		this.webServiceManager = manager;
 	}
 
-	/**
-	 * Cancels this task by cancelling the {@link WebServiceRequest} and then
-	 * cancelling this {@link WebServiceRequestAsyncTask}. 
-	 */
-	public void cancelRequest() {
-		request.cancel();
-		this.cancel(true);
-	}
-	
 	@Override
-	protected ResultInfo<ResultType> doInBackground(Void... params) {
-		final BaseWebServiceProgress progress = new BaseWebServiceProgress(-1, -1);
-		request.addProgressListener(new ProgressListener() {
-			@Override
-			public void onProgressUpdate(long currentProgress, long maxProgress) {
-				progress.currentProgress = currentProgress;
-				progress.maxProgress = maxProgress;
-				publishProgress(progress);
-			}
-		});
-		
-		if (isCancelled()) {
-			request.cancel();
-		}
-		
-		return webServiceManager.doRequest(request, requestMode);
+	protected WebServiceRequest<ResultType> createRequest() {
+		return request;
 	}
+
 }
