6fa5cde1a03915df7052a85692fbf1fd66f708ad
Joe Mahon <joe.mahon@raizlabs.com>
2013-11-22 12:13:54 -0500

    Throw more descriptive Exception if URLEncoder is about to fail

--- WebServiceManager/src/com/raizlabs/net/requests/RequestBuilder.java
+++ WebServiceManager/src/com/raizlabs/net/requests/RequestBuilder.java
@@ -353,17 +353,26 @@ public class RequestBuilder {
 		}
 		return pairs;
 	}
-
+	
 	private String getQueryString(Map<String, String> map) {
 		StringBuilder queryBuilder = new StringBuilder();
 		boolean first = true;
 		for (Entry<String, String> entry : map.entrySet()) {
+			// This will throw a NullPointerException if you call URLEncoder.encode(null). 
+			// Instead caught & thrown with description above.
+			String value = entry.getValue();
+			if (value == null) {
+				// Can't be more specific without jeopardizing security.
+				throw new NullPointerException("Malformed Request. RequestBuilder entry " +
+						"has null value for key "+entry.getKey()+" on URI "+this.uri+".");
+			}
+			
 			if (!first) {
 				queryBuilder.append("&");
 			}
 			queryBuilder.append(entry.getKey());
 			queryBuilder.append("=");
-			queryBuilder.append(URLEncoder.encode(entry.getValue()));
+			queryBuilder.append(URLEncoder.encode(value));
 
 			first = false;
 
